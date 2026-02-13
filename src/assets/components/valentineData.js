f1662178c389fcce711388a68190efab02e062ec
Your Name <lance1022angelo@gmail.com>
2026-02-13 11:56:31 +0800

    Describe what you changed

---
+++ src/assets/components/valentineData.js
@@ -0,0 +1,43 @@
+export const SAFE_MARGIN = 0.15;
+export const BTN_WIDTH = 150;
+export const BTN_HEIGHT = 50;
+
+export const YES_BASE = 16;
+export const YES_STEP = 8;
+export const YES_MAX = 80;
+
+export const phases = [
+  {
+    text: "Will you be my Valentine?",
+    image: "/images/Cutesy cat.jpg",
+    emoji: "💌",
+  },
+  {
+    text: "Sure naba yan?",
+    image: "/images/Sad Hamster Sticker.gif",
+  },
+  {
+    text: "Talaga Ba? 😰",
+    image: "/images/Sad Face Sticker.gif",
+  },
+  {
+    text: "Pag isipan mo naman mabuti ah",
+    image: "/images/Sad Cat Sticker by Capoo.gif",
+  },
+  {
+    text: "LALAGYAN KITA NG MALWARE PAG UMAYAW KAPA",
+    image: "/images/Sad Cat Sticker by MYAOWL.gif",
+  },
+  {
+    text: "LAST CHANCE HAHAHAHA",
+    image: "/images/Baby Meme GIF.gif",
+  },
+  {
+    text: "PIDI YAN KASI",
+    image: "/images/Sad Neon Genesis Evangelion Sticker by Castaways.gif",
+  },
+  {
+    text: "PINDUTIN MO NALANG YUNG YES PLEASE",
+    image: "/images/Sticker ねこ Sticker by Japan.gif",
+  },
+];
